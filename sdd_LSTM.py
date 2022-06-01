dadb329dec5fd8eb2cb48037b292cc689d580b25
Swapnil D Daphal <daphalsd01@gmail.com>
2022-06-01 12:47:59 +0530

    Update sdd_LSTM.py

--- sdd_LSTM.py
+++ sdd_LSTM.py
@@ -19,3 +19,20 @@ model.summary()
 
 # Load the mnist dataset for the problem in hand
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+# Normalize the input data for the better results
+x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train = x_train.astype(np.float32)
+x_test =  x_test.astype(np.float32)
+
+_FAST_TRAIN = False
+_EPOCHS = 5
+
+if _FAST_TRAIN:
+  _EPOCHS = 1
+  _TRAINING_DATA_COUNT = 1000
+  x_train = x_train[:_TRAINING_DATA_COUNT]
+  y_train = y_train[:_TRAINING_DATA_COUNT]
+  
+model.fit(x_train, y_train, epochs=_EPOCHS)
+model.evaluate(x_test, y_test, verbose=0)
+
