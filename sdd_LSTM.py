df0a6f2e060c064315a149b90e5fc4c5c775f783
Swapnil D Daphal <daphalsd01@gmail.com>
2022-06-01 12:33:34 +0530

    Update sdd_LSTM.py

--- sdd_LSTM.py
+++ sdd_LSTM.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
+# creates the representative dataset useful for uint8/int8 qunatization options
 def representative_dataset():
   for _ in range(100):
     data = np.random.rand(1, 28, 28)
@@ -8,6 +9,7 @@ def representative_dataset():
     
 model = tf.keras.models.Sequential([
   tf.keras.layers.Input(shape=(28,28), name='input'),
-  
-
+  tf.keras.layers.LSTM(20, time_major=False, return_sequences=True),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(10, activation='softmax', name='output')
 ])
